239b803f73ad75a64010ad0f12abf96cff478536
David Seddon <david.seddon@octoenergy.com>
2018-12-12 11:29:03 +0000

    Release v1.0b2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*names, **kwargs):
 
 setup(
     name='grimp',
-    version='1.0b1',
+    version='1.0b2',
     license='BSD 2-Clause License',
     description="Builds a graph of a Python project's internal dependencies.",
     long_description=read('README.rst'),
