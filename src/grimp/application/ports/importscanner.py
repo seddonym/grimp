c9f8e47edce1c29e1f04291a1fcd7c28cab3fe14
David Seddon <david.seddon@octoenergy.com>
2018-12-20 12:15:48 +0000

    Add include_external_packages to AbstractImportScanner constructor

--- src/grimp/application/ports/importscanner.py
+++ src/grimp/application/ports/importscanner.py
@@ -12,17 +12,21 @@ class AbstractImportScanner(abc.ABC):
     def __init__(self,
                  modules: Set[Module],
                  package_directory: str,
-                 file_system: AbstractFileSystem) -> None:
+                 file_system: AbstractFileSystem,
+                 include_external_packages: bool = False) -> None:
         """
         Args:
             - modules:           All the modules in the package we are scanning.
             - package_directory: The full file path of the directory of the package that
                                  contains all the modules are in, for example '/path/to/mypackage'.
             - file_system:       The file system interface to use.
+            - include_external_packages: Whether to include imports of external packages
+                                         in the results.
         """
         self.modules = modules
         self.package_directory = package_directory
         self.file_system = file_system
+        self.include_external_packages = include_external_packages
 
     @abc.abstractmethod
     def scan_for_imports(self, module: Module) -> Set[DirectImport]:
