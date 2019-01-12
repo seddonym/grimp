5bef1b88c2988aaabe446a520def7dabca40ac16
David Seddon <david.seddon@octoenergy.com>
2019-01-12 12:00:25 +0000

    Remove ImportChain object

--- tests/unit/domain/test_valueobjects.py
+++ tests/unit/domain/test_valueobjects.py
@@ -1,4 +1,4 @@
-from grimp.domain.valueobjects import Module, DirectImport, ImportChain
+from grimp.domain.valueobjects import Module, DirectImport
 
 
 class TestModule:
@@ -125,55 +125,3 @@ class TestDirectImport:
         assert hash(a) != hash(d)
         assert hash(a) != hash(e)
         assert hash(a) != hash(f)
-
-
-class TestImportChain:
-    def test_repr(self):
-        import_path = ImportChain(
-            Module('one'),
-            Module('two'),
-            Module('three'),
-        )
-        assert repr(import_path) == '<ImportChain: three -> two -> one>'
-
-    def test_equals(self):
-        a = ImportChain(
-            Module('one'),
-            Module('two'),
-            Module('three'),
-        )
-        b = ImportChain(
-            Module('one'),
-            Module('two'),
-            Module('three'),
-        )
-        c = ImportChain(
-            Module('one'),
-            Module('three'),
-            Module('two'),
-        )
-
-        assert a == b
-        assert a != c
-        # Also non-ImportChain instances should not be treated as equal.
-        assert a != 'foo'
-
-    def test_hash(self):
-        a = ImportChain(
-            Module('one'),
-            Module('two'),
-            Module('three'),
-        )
-        b = ImportChain(
-            Module('one'),
-            Module('two'),
-            Module('three'),
-        )
-        c = ImportChain(
-            Module('one'),
-            Module('three'),
-            Module('two'),
-        )
-
-        assert hash(a) == hash(b)
-        assert hash(a) != hash(c)
