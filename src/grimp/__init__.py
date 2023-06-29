5e8e180252a8e6c02f4e56043608be18fe6b7cfd
David Seddon <david.seddon@octoenergy.com>
2023-06-29 12:43:58 +0100

    Stub out analyze_layers

--- src/grimp/__init__.py
+++ src/grimp/__init__.py
@@ -1,7 +1,16 @@
 __version__ = "2.4"
 
 from .application.ports.graph import DetailedImport, ImportGraph
+from .domain.analysis import PackageDependency, Route
 from .domain.valueobjects import DirectImport, Module
 from .main import build_graph
 
-__all__ = ["Module", "DetailedImport", "DirectImport", "ImportGraph", "build_graph"]
+__all__ = [
+    "Module",
+    "DetailedImport",
+    "DirectImport",
+    "ImportGraph",
+    "PackageDependency",
+    "Route",
+    "build_graph",
+]
