37999ba52a886711e071f311465e14f538caccbd
David Seddon <david.seddon@octoenergy.com>
2024-04-19 17:49:29 +0100

    Add some basic benchmarking tools

---
+++ tests/benchmarking/test_benchmarking.py
@@ -0,0 +1,46 @@
+import pytest
+import json
+from pathlib import Path
+from grimp.adaptors.graph import ImportGraph
+
+
+@pytest.fixture(scope="module")
+def large_graph():
+    raw_json = (Path(__file__).parent / "large_graph.json").read_text()
+    graph_dict = json.loads(raw_json)
+    graph = ImportGraph()
+
+    for importer, importeds in graph_dict.items():
+        for imported in importeds:
+            graph.add_import(importer=importer, imported=imported)
+
+    return graph
+
+
+def test_top_level_large_graph(large_graph, benchmark):
+    benchmark(
+        lambda: large_graph.find_illegal_dependencies_for_layers(
+            layers=("plugins", "application", "domain", "data"),
+            containers=("mypackage",),
+        )
+    )
+
+
+def test_deep_layers_large_graph(large_graph, benchmark):
+    layers = (
+        "mypackage.plugins.5634303718.1007553798.8198145119.application.3242334296.1991886645",
+        "mypackage.plugins.5634303718.1007553798.8198145119.application.3242334296.6397984863",
+        "mypackage.plugins.5634303718.1007553798.8198145119.application.3242334296.9009030339",
+        "mypackage.plugins.5634303718.1007553798.8198145119.application.3242334296.6666171185",
+        "mypackage.plugins.5634303718.1007553798.8198145119.application.3242334296.1693068682",
+        "mypackage.plugins.5634303718.1007553798.8198145119.application.3242334296.1752284225",
+        "mypackage.plugins.5634303718.1007553798.8198145119.application.3242334296.9089085203",
+        "mypackage.plugins.5634303718.1007553798.8198145119.application.3242334296.5033127033",
+        "mypackage.plugins.5634303718.1007553798.8198145119.application.3242334296.2454157946",
+    )
+    benchmark.pedantic(
+        lambda: large_graph.find_illegal_dependencies_for_layers(
+            layers=layers,
+        ),
+        rounds=3,
+    )
