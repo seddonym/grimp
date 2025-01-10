dc3e9c5115ec24ba70c2edf0a20244f536aa7b32
David Seddon <david.seddon@octoenergy.com>
2025-01-10 10:12:35 +0000

    Try benchmarking

--- tests/benchmarking/test_benchmarking.py
+++ tests/benchmarking/test_benchmarking.py
@@ -17,36 +17,36 @@ def large_graph():
     return graph
 
 
-def test_top_level_large_graph(large_graph, benchmark):
-    benchmark(
-        lambda: large_graph.find_illegal_dependencies_for_layers(
-            layers=("plugins", "application", "domain", "data"),
-            containers=("mypackage",),
-        )
-    )
-
-
-def test_deep_layers_large_graph(large_graph, benchmark):
-    layers = (
-        "mypackage.plugins.5634303718.1007553798.8198145119.application.3242334296.1991886645",
-        "mypackage.plugins.5634303718.1007553798.8198145119.application.3242334296.6397984863",
-        "mypackage.plugins.5634303718.1007553798.8198145119.application.3242334296.9009030339",
-        "mypackage.plugins.5634303718.1007553798.8198145119.application.3242334296.6666171185",
-        "mypackage.plugins.5634303718.1007553798.8198145119.application.3242334296.1693068682",
-        "mypackage.plugins.5634303718.1007553798.8198145119.application.3242334296.1752284225",
-        "mypackage.plugins.5634303718.1007553798.8198145119.application.3242334296.9089085203",
-        "mypackage.plugins.5634303718.1007553798.8198145119.application.3242334296.5033127033",
-        "mypackage.plugins.5634303718.1007553798.8198145119.application.3242334296.2454157946",
-    )
-    fn = lambda: large_graph.find_illegal_dependencies_for_layers(
-        layers=layers,
-    )
-    if hasattr(benchmark, "pendantic"):
-        # Running with pytest-benchmark
-        benchmark.pedantic(fn, rounds=3)
-    else:
-        # Running with codspeed.
-        benchmark(fn)
+# def test_top_level_large_graph(large_graph, benchmark):
+#     benchmark(
+#         lambda: large_graph.find_illegal_dependencies_for_layers(
+#             layers=("plugins", "application", "domain", "data"),
+#             containers=("mypackage",),
+#         )
+#     )
+#
+#
+# def test_deep_layers_large_graph(large_graph, benchmark):
+#     layers = (
+#         "mypackage.plugins.5634303718.1007553798.8198145119.application.3242334296.1991886645",
+#         "mypackage.plugins.5634303718.1007553798.8198145119.application.3242334296.6397984863",
+#         "mypackage.plugins.5634303718.1007553798.8198145119.application.3242334296.9009030339",
+#         "mypackage.plugins.5634303718.1007553798.8198145119.application.3242334296.6666171185",
+#         "mypackage.plugins.5634303718.1007553798.8198145119.application.3242334296.1693068682",
+#         "mypackage.plugins.5634303718.1007553798.8198145119.application.3242334296.1752284225",
+#         "mypackage.plugins.5634303718.1007553798.8198145119.application.3242334296.9089085203",
+#         "mypackage.plugins.5634303718.1007553798.8198145119.application.3242334296.5033127033",
+#         "mypackage.plugins.5634303718.1007553798.8198145119.application.3242334296.2454157946",
+#     )
+#     fn = lambda: large_graph.find_illegal_dependencies_for_layers(
+#         layers=layers,
+#     )
+#     if hasattr(benchmark, "pendantic"):
+#         # Running with pytest-benchmark
+#         benchmark.pedantic(fn, rounds=3)
+#     else:
+#         # Running with codspeed.
+#         benchmark(fn)
 
 
 def test_build_django(benchmark):
