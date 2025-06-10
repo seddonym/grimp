f17a172a1315cd4bb227104575cae618a52d7127
David Seddon <david.seddon@kraken.tech>
2025-06-10 15:44:19 +0100

    Make AbstractImportScanner a Protocol

--- src/grimp/adaptors/importscanner.py
+++ src/grimp/adaptors/importscanner.py
@@ -6,8 +6,8 @@ from dataclasses import dataclass
 from typing import Dict, Optional, Set
 
 from grimp import exceptions
-from grimp.application.ports.importscanner import AbstractImportScanner
 from grimp.application.ports.modulefinder import FoundPackage
+from grimp.application.ports.filesystem import BasicFileSystem
 from grimp.domain.valueobjects import DirectImport, Module
 from grimp import _rustgrimp as rust  # type: ignore[attr-defined]
 
@@ -25,9 +25,30 @@ class _ImportedObject:
     typechecking_only: bool
 
 
-class ImportScanner(AbstractImportScanner):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
+class ImportScanner:
+    def __init__(
+        self,
+        file_system: BasicFileSystem,
+        found_packages: Set[FoundPackage],
+        include_external_packages: bool = False,
+    ) -> None:
+        """
+        Args:
+            - found_packages:                Set of FoundPackages containing all the modules
+                                             for analysis.
+            - file_system:                   The file system interface to use.
+            - include_external_packages:     Whether to include imports of external modules (i.e.
+                                             modules not contained in modules_by_package_directory)
+                                             in the results.
+        """
+        self.file_system = file_system
+        self.include_external_packages = include_external_packages
+        self.found_packages = found_packages
+
+        # Flatten all the modules into a set.
+        self.modules: Set[Module] = set()
+        for package in self.found_packages:
+            self.modules |= {mf.module for mf in package.module_files}
 
         self._found_packages_by_module: Dict[Module, FoundPackage] = {
             module_file.module: package
