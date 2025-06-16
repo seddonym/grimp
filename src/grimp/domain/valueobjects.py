aaeaf6c629c555b9dc9ad285f5fd43dd00a0cfce
Peter Byfield <peter.byfield@kraken.tech>
2025-06-16 21:08:23 +0200

    Adapt value objects to use dataclasses

--- src/grimp/domain/valueobjects.py
+++ src/grimp/domain/valueobjects.py
@@ -1,10 +1,14 @@
-from typing import Any
+from dataclasses import dataclass, astuple
+from typing import Set, Any
 
 
+@dataclass(frozen=True, repr=False, eq=False)
 class ValueObject:
     def __repr__(self) -> str:
         return "<{}: {}>".format(self.__class__.__name__, self)
 
+    # We use a custom __eq__ method to enforce the liskov principle.
+    # e.g. SpecialModule("foo") == Module("foo") where SpecialModule is a subclass of Module.
     def __eq__(self, other: Any) -> bool:
         if isinstance(other, type(self)) or isinstance(self, type(other)):
             return hash(self) == hash(other)
@@ -12,22 +16,17 @@ class ValueObject:
             return False
 
     def __hash__(self) -> int:
-        return hash(str(self))
+        return hash(astuple(self))
 
 
+@dataclass(frozen=True, repr=False, eq=False)
 class Module(ValueObject):
     """
     A Python module.
     """
 
-    __slots__ = ("name",)
-
-    def __init__(self, name: str) -> None:
-        """
-        Args:
-            name: The fully qualified name of a Python module, e.g. 'package.foo.bar'.
-        """
-        self.name = name
+    # The fully qualified name of a Python module, e.g. 'package.foo.bar'.
+    name: str
 
     def __str__(self) -> str:
         return self.name
@@ -61,31 +60,22 @@ class Module(ValueObject):
         return self.name.startswith(f"{module.name}.")
 
 
+@dataclass(frozen=True, repr=False, eq=False)
 class DirectImport(ValueObject):
     """
     An import between one module and another.
     """
 
-    def __init__(
-        self,
-        *,
-        importer: Module,
-        imported: Module,
-        line_number: int,
-        line_contents: str,
-    ) -> None:
-        self.importer = importer
-        self.imported = imported
-        self.line_number = line_number
-        self.line_contents = line_contents
+    importer: Module
+    imported: Module
+    line_number: int
+    line_contents: str
 
     def __str__(self) -> str:
-        return "{} -> {} (l. {})".format(self.importer, self.imported, self.line_number)
-
-    def __hash__(self) -> int:
-        return hash((str(self), self.line_contents))
+        return f"{self.importer} -> {self.imported} (l. {self.line_number})"
 
 
+@dataclass(frozen=True, repr=False, eq=False)
 class Layer(ValueObject):
     """
     A layer within a layered architecture.
@@ -94,13 +84,14 @@ class Layer(ValueObject):
     independent. This is the default.
     """
 
-    def __init__(
-        self,
-        *module_tails: str,
-        independent: bool = True,
-    ) -> None:
-        self.module_tails = set(module_tails)
-        self.independent = independent
+    module_tails: Set[str]
+    independent: bool
+
+    # A custom `__init__` is needed since `module_tails` is a variadic argument.
+    def __init__(self, *module_tails: str, independent: bool = True) -> None:
+        # `object.__setattr__` is needed since the dataclass is frozen.
+        object.__setattr__(self, "module_tails", set(module_tails))
+        object.__setattr__(self, "independent", independent)
 
     def __str__(self) -> str:
         return f"{self.module_tails}, independent={self.independent}"
